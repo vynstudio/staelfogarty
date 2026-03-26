c13437333513784b96c54f39c8c0ec4466273f14
Vyn Studio <fix@vynstudio.com>
2026-03-26 23:37:13 +0000

    Rename: 1/1 Private Lessons → One-on-One Private Lessons across all pages and functions

--- netlify/functions/create-zoom-meeting.js
+++ netlify/functions/create-zoom-meeting.js
@@ -13,7 +13,7 @@
 const { google } = require('googleapis');
 const { Resend } = require('resend');
 
-const VIRTUAL_SERVICES = ['Remote Interpretation', '1/1 Private Lessons', 'Citizenship Test Preparation', 'Legal Services'];
+const VIRTUAL_SERVICES = ['Remote Interpretation', 'One-on-One Private Lessons', 'Citizenship Test Preparation', 'Legal Services'];
 const STAEL_EMAIL = process.env.STAEL_EMAIL || 'hello@staelfogarty.com';
 
 exports.handler = async (event) => {
